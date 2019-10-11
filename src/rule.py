2a3934ab21f523d4ac8f1d40cd4dd36536c76800
hakonbyh <56154860+hakonbyh@users.noreply.github.com>
2019-10-11 10:43:04 +0200

    updated and working with function signal_is_digit and all_symbols

---
+++ src/rule.py
@@ -0,0 +1,23 @@
+def signal_is_digit(signal):
+    return 48 <= ord(signal) <= 57
+
+def all_symbols(signal):
+    return True
+
+from inspect import isfunction
+
+class Rule:
+
+    def __init__(self, state1, state2, signal, action):
+        self.state1 = state1
+        self.state2 = state2
+        self.signal = signal
+        self.action = action
+
+    def match(self, state, symbol):
+        if state == self.state1:
+            if isfunction(self.signal):
+                return self.signal(str(symbol))
+            else:
+                return symbol == self.signal
+        return False
