62d9165441f42cd7fc21dc1c218bee3619174b22
bardurion <olstad1995@gmail.com>
2019-10-14 14:39:34 +0200

    linted led_board.py

--- src/led_board.py
+++ src/led_board.py
@@ -1,22 +1,22 @@
 '''The class handling the LED board'''
 
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
 
 
 class LED:
     '''The lED class'''
 
     def __init__(self):
-        self.PIN0 = 6
-        self.PIN1 = 13
-        self.PIN2 = 19
-        self.single_led_handling = {0: (self.PIN2, self.PIN0, self.PIN1, self.PIN0, self.PIN1),
-                                    1: (self.PIN2, self.PIN0, self.PIN1, self.PIN1, self.PIN0),
-                                    2: (self.PIN0, self.PIN1, self.PIN2, self.PIN1, self.PIN2),
-                                    3: (self.PIN0, self.PIN1, self.PIN2, self.PIN2, self.PIN1),
-                                    4: (self.PIN1, self.PIN0, self.PIN2, self.PIN0, self.PIN2),
-                                    5: (self.PIN1, self.PIN0, self.PIN2, self.PIN2, self.PIN0)
+        self.pin0 = 6
+        self.pin1 = 13
+        self.pin2 = 19
+        self.single_led_handling = {0: (self.pin2, self.pin0, self.pin1, self.pin0, self.pin1),
+                                    1: (self.pin2, self.pin0, self.pin1, self.pin1, self.pin0),
+                                    2: (self.pin0, self.pin1, self.pin2, self.pin1, self.pin2),
+                                    3: (self.pin0, self.pin1, self.pin2, self.pin2, self.pin1),
+                                    4: (self.pin1, self.pin0, self.pin2, self.pin0, self.pin2),
+                                    5: (self.pin1, self.pin0, self.pin2, self.pin2, self.pin0)
                                     }
 
     def setup(self):
@@ -24,8 +24,9 @@ class LED:
         GPIO.setmode(GPIO.BCM)
 
     def light_led(self, led_id):
-        """Turn on one of the 6 LEDs by making the appropriate #combination of input and output declarations,
-        and #then making the appropriate HIGH / LOW settings on #the output pins."""
+        """Turn on one of the 6 LEDs by making the appropriate
+        combination of input and output declarations, and then
+        making the appropriate HIGH / LOW settings on #the output pins."""
         led_order = self.single_led_handling[led_id]
         GPIO.setup(led_order[0], GPIO.IN)
         GPIO.setup(led_order[1], GPIO.OUT)
@@ -35,12 +36,13 @@ class LED:
 
     def light_off_led(self):
         """Turn off all LEDs"""
-        GPIO.setup(self.PIN0, GPIO.IN)
-        GPIO.setup(self.PIN1, GPIO.IN)
-        GPIO.setup(self.PIN2, GPIO.IN)
+        GPIO.setup(self.pin0, GPIO.IN)
+        GPIO.setup(self.pin1, GPIO.IN)
+        GPIO.setup(self.pin2, GPIO.IN)
 
     def flash_all_leds(self, led_duration):
-        """Flash all 6 LEDs on and off for k seconds, where k is #an argument of the method."""
+        """Flash all 6 LEDs on and off for k seconds,
+        where k is an argument of the method."""
         timeout = led_duration  # [seconds]
         timeout_start = time.time()
         while time.time() < timeout_start + timeout:
@@ -49,7 +51,8 @@ class LED:
         self.light_off_led()
 
     def twinkle_all_leds(self, led_duration):
-        """Turn all LEDs on and off in sequence for k seconds, #where k is an argument of the method."""
+        """Turn all LEDs on and off in sequence for k seconds,
+        where k is an argument of the method."""
         timeout = led_duration  # [seconds]
         timeout_start = time.time()
         while time.time() < timeout_start + timeout:
@@ -78,6 +81,6 @@ class LED:
 
 
 if __name__ == '__main__':
-    led = LED()
-    led.setup()
-    led.leds_powering_down()
+    TEST_LED = LED()
+    TEST_LED.setup()
+    TEST_LED.leds_powering_down()
