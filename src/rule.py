2e3147c390cbb48b3e84f4f9f365c62fce9a6197
bardurion <olstad1995@gmail.com>
2019-10-14 14:29:30 +0200

    linted rule.py

--- src/rule.py
+++ src/rule.py
@@ -1,12 +1,19 @@
-def signal_is_digit(signal):
-    return 48 <= ord(signal) <= 57
-
-def all_symbols(signal):
-    return True
-
+'''The rule navigation class'''
 from inspect import isfunction
 
+
+def signal_is_digit(signal):
+    '''Validates that signal is an integer 0 - 9'''
+    return 48 <= ord(signal) <= 57
+
+
+def all_symbols():
+    '''Acknowledges that all input is signal'''
+    return True
+
+
 class Rule:
+    '''The rule class'''
 
     def __init__(self, state1, state2, signal, action):
         self.state1 = state1
@@ -15,9 +22,9 @@ class Rule:
         self.action = action
 
     def match(self, state, symbol):
+        '''Checks if correct state, and execute operation'''
         if state == self.state1:
             if isfunction(self.signal):
                 return self.signal(str(symbol))
-            else:
-                return symbol == self.signal
+            return symbol == self.signal
         return False
